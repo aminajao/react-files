c01b971b60f6f1ba21dd13c02b302c60651fed27
Bilal <39632030+aminajao@users.noreply.github.com>
2021-04-03 11:01:23 +0100

    Initial commit

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AppBar = () => {
+  return <div>I am the App Bar</div>;
+};
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const NavBar = () => {
+  return <div>I am the Nav Bar</div>;
+};
